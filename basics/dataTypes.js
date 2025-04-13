00f43274ef96100689b7eb342d8edf572e7e29ac
DishaVerma-20 <ms.dishaverma.20@gmail.com>
2025-04-13 17:02:50 +0000

    dataTypes

---
+++ basics/dataTypes.js
@@ -0,0 +1,34 @@
+"use strict"; // treat all JS Code as newer version
+// itna likhne ki must jrurat nahi hai but yes its a gud practice..
+
+// DOCUMENTATIONS 
+/*
+MDN easy way ma likha hua hai..
+for this specifically ECMA standards tc39.es
+*/
+
+//alert(3+3)  // message box hota hai yeh.. but here we are using node.js not browser
+// this is particularly for web browsers.. here, it will show error
+
+// code should have high readability
+console.log(3
+    +
+    3
+)// ese error nahi hoga but it is not readable
+
+let name = 'DishaVerma'
+console.log(name)
+
+// Data Types - Primitive
+/*
+number => 2 ka power 53 tk.. not sure 
+bigint
+string => "" '' double quotes or single quotes we need to use
+boolean => true/false
+null => standalone value object type ma aata esa kuch scene hai iska
+undefined
+symbol => unique figma jese tools mai kaafi use hai iska
+*/
+console.log(typeof null) // object
+console.log(typeof undefined); // undefined
+console.log(typeof "DishaVerma")
